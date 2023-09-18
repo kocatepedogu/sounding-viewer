1c8efc05226c80fdc66fb26c19ba9739f1558d31
Doğu Kocatepe <93269312+kocatepedogu@users.noreply.github.com>
2023-09-18 18:09:42 +0300

    GNU GPL license notices are added

--- src/renderer/rucsoundings.ts
+++ src/renderer/rucsoundings.ts
@@ -1,5 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+/*
+ * Copyright 2023 Doğu Kocatepe
+ * This file is part of Sounding Viewer.
+
+ * Sounding Viewer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Sounding Viewer is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with Sounding Viewer. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /**
  * This file contains functions for fetching and parsing GSD sounding format described in the link below.
  * https://rucsoundings.noaa.gov/raob_format.html
